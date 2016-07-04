54db3677d69fbf0b4798fc6dcc6239f69823d162
bsautermeister <benjamin.sautermeister@tum.de>
2016-07-04 18:04:31 +0200

    Sourrounded encoder and decoder with name scope

--- conv-imseq/model/conv_deconv_model.py
+++ conv-imseq/model/conv_deconv_model.py
@@ -8,59 +8,64 @@ import tensorflow as tf
 
 
 def inference(stacked_input, FRAME_CHANNELS, INPUT_SEQ_LENGTH, LAMBDA):
-    # conv1  
-    conv1 = tt.network.conv2d("conv1", stacked_input,
-                              64, 5, 5, 2, 2,
-                              weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
-                              bias=0.1,
-                              regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
-                              activation=tf.nn.relu)
-    tt.board.activation_summary(conv1)
+    with tf.name_scope("encoder"):
+        # conv1  
+        conv1 = tt.network.conv2d("conv1", stacked_input,
+                                  64, 5, 5, 2, 2,
+                                  weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
+                                  bias=0.1,
+                                  regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
+                                  activation=tf.nn.relu)
+        tt.board.activation_summary(conv1)
+        tt.board.conv_image_summary("conv1_out", conv1)
+
+        # conv2  
+        conv2 = tt.network.conv2d("conv2", conv1,
+                                  128, 5, 5, 2, 2,
+                                  weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
+                                  bias=0.1,
+                                  regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
+                                  activation=tf.nn.relu)
+        tt.board.activation_summary(conv2)
+        tt.board.conv_image_summary("conv2_out", conv2)
+
+        # conv3       
+        conv3 = tt.network.conv2d("conv3", conv2,
+                                  256, 5, 5, 2, 2,
+                                  weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
+                                  bias=0.1,
+                                  regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
+                                  activation=tf.nn.relu)
+        tt.board.activation_summary(conv3)
+        tt.board.conv_image_summary("conv3_out", conv3)
     
-    # conv2  
-    conv2 = tt.network.conv2d("conv2", conv1,
-                              128, 5, 5, 2, 2,
-                              weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
-                              bias=0.1,
-                              regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
-                              activation=tf.nn.relu)
-    tt.board.activation_summary(conv2)
-    
-    # conv3       
-    conv3 = tt.network.conv2d("conv3", conv2,
-                              256, 5, 5, 2, 2,
-                              weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
-                              bias=0.1,
-                              regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
-                              activation=tf.nn.relu)
-    tt.board.activation_summary(conv3)
-    
-    # conv_tp4
-    conv_tp4 = tt.network.conv2d_transpose("conv_tp4", conv3,
-                                           128, 5, 5, 2, 2,
-                                           weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
-                                           bias=0.1,
-                                           regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
-                                           activation=tf.nn.relu)
-    tt.board.activation_summary(conv_tp4)
-    
-    # conv_tp5  
-    conv_tp5 = tt.network.conv2d_transpose("conv_tp5", conv_tp4,
-                                           64, 5, 5, 2, 2,
-                                           weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
-                                           bias=0.1,
-                                           regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
-                                           activation=tf.nn.relu)
-    tt.board.activation_summary(conv_tp5)
-    
-    # conv_tp6       
-    conv_tp6 = tt.network.conv2d_transpose("conv_tp6", conv_tp5,
-                                           FRAME_CHANNELS, 5, 5, 2, 2,
-                                           weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
-                                           bias=0.1,
-                                           regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
-                                           activation=tf.nn.relu)
-    tt.board.activation_summary(conv_tp6)
+    with tf.name_scope("decoder"):
+        # conv_tp4
+        conv_tp4 = tt.network.conv2d_transpose("conv_tp4", conv3,
+                                               128, 5, 5, 2, 2,
+                                               weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
+                                               bias=0.1,
+                                               regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
+                                               activation=tf.nn.relu)
+        tt.board.activation_summary(conv_tp4)
+
+        # conv_tp5  
+        conv_tp5 = tt.network.conv2d_transpose("conv_tp5", conv_tp4,
+                                               64, 5, 5, 2, 2,
+                                               weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
+                                               bias=0.1,
+                                               regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
+                                               activation=tf.nn.relu)
+        tt.board.activation_summary(conv_tp5)
+
+        # conv_tp6       
+        conv_tp6 = tt.network.conv2d_transpose("conv_tp6", conv_tp5,
+                                               FRAME_CHANNELS, 5, 5, 2, 2,
+                                               weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
+                                               bias=0.1,
+                                               regularizer=tf.contrib.layers.l2_regularizer(LAMBDA),
+                                               activation=tf.nn.relu)
+        tt.board.activation_summary(conv_tp6)
         
     return conv_tp6
 
