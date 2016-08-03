f1b4aaf30fb0c98cf1e713349909e4d546b87f6c
bsautermeister <benjamin.sautermeister@tum.de>
2016-08-03 11:02:43 +0200

    Implemented frame prediction inference
    Using no ground truth frames, but the output of the previous prediction.

--- conv-imseq/model/lstmconv2d_encoder_decoder.py
+++ conv-imseq/model/lstmconv2d_encoder_decoder.py
@@ -2,25 +2,29 @@ import tensortools as tt
 import tensorflow as tf
 
 
-def conv2d_lstm(image_list, initial_state):
+def _create_lstm_cell():
     LSTM_LAYERS = 1
     LSTM_KSIZE = 7
     LSTM_FILTERS = 32
     LSTM_HEIGHT = 64
     LSTM_WIDTH = 64
-    
     lstm_cell = tt.recurrent.BasicLSTMConv2DCell(LSTM_KSIZE, LSTM_KSIZE,
                                                  LSTM_FILTERS, LSTM_HEIGHT, LSTM_WIDTH,
                                                  device='/cpu:0')
     if LSTM_LAYERS > 1:
         lstm_cell = tt.recurrent.MultiRNNConv2DCell([lstm_cell] * LSTM_LAYERS)
+    return lstm_cell
+    
+
+def _conv2d_lstm(image_list, initial_state):
+    lstm_cell = _create_lstm_cell()
         
     # Get lstm cell output
     outputs, states = tt.recurrent.rnn_conv2d(lstm_cell, image_list, initial_state=initial_state)
-    return outputs, states    
-    
+    return outputs, states      
 
-def inference(input_seq, pred_seq, FRAME_CHANNELS, INPUT_SEQ_LENGTH, LAMBDA):
+
+def inference(input_seq, pred_seq, FRAME_CHANNELS, INPUT_SEQ_LENGTH, LAMBDA): # TODO: make INPUT_SEQ_LENGTH ==> OUTPUT_SEQ_LENGT param?
     # input_seq: [batch_size, n_steps, h, w, c]
     batch_size = tf.shape(input_seq)[0]
     static_input_shape = input_seq.get_shape().as_list()
@@ -29,31 +33,54 @@ def inference(input_seq, pred_seq, FRAME_CHANNELS, INPUT_SEQ_LENGTH, LAMBDA):
     input_seq = [tf.squeeze(i, (0,)) for i in input_seq]
     
     with tf.variable_scope("encoder-lstm"):
-        enc_outputs, enc_state = conv2d_lstm(input_seq, None)
+        enc_outputs, enc_state = _conv2d_lstm(input_seq, None)
     
     # conditional future predictor
-    # on training:  use ground truth previous frames
-    # on test/eval: use previously generated frame (TODO!!!)  
-    static_pred_shape = pred_seq.get_shape().as_list()
-    pred_seq = tf.transpose(pred_seq, [1, 0, 2, 3, 4])
-    pred_seq = tf.split(0, static_pred_shape[1], pred_seq)
-    pred_seq_list = [input_seq[-1]]
-    for i in xrange(static_pred_shape[1] - 1):
-        pred_seq_list.append(tf.squeeze(pred_seq[i], (0,)))
-        
-    with tf.variable_scope('decoder-lstm'):
-        dec_outputs, _ = conv2d_lstm(pred_seq_list, enc_state)
-        
-        for i in xrange(len(dec_outputs)):
-            dec_outputs[i] = tt.network.conv2d("Conv-Reduce", dec_outputs[i], 1,
+    if pred_seq is not None:
+        # on training:  use ground truth previous frames 
+        static_pred_shape = pred_seq.get_shape().as_list()
+        pred_seq = tf.transpose(pred_seq, [1, 0, 2, 3, 4])
+        pred_seq = tf.split(0, static_pred_shape[1], pred_seq)
+        pred_seq_list = [input_seq[-1]]
+        for i in xrange(static_pred_shape[1] - 1):
+            pred_seq_list.append(tf.squeeze(pred_seq[i], (0,)))
+
+        with tf.variable_scope('decoder-lstm'):
+            dec_outputs, _ = _conv2d_lstm(pred_seq_list, enc_state)
+
+            for i in xrange(len(dec_outputs)):
+                dec_outputs[i] = tt.network.conv2d("Conv-Reduce", dec_outputs[i], 1,
+                                                   3, 3, 1, 1,
+                                                   weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
+                                                   bias_init=0.1,
+                                                   regularizer=tf.contrib.layers.l2_regularizer(LAMBDA))
+                tf.get_variable_scope().reuse_variables()
+
+        model_outputs = tf.pack(dec_outputs, axis=1)
+    else:
+        # on test/eval: use previously generated frame
+        with tf.variable_scope('decoder-lstm') as varscope:
+            lstm_cell = _create_lstm_cell()
+            input_ = input_seq[-1]
+            state = enc_state
+            dec_outputs = []
+            
+            for time in xrange(INPUT_SEQ_LENGTH): # FIXME output seq length
+                if time > 0:
+                        varscope.reuse_variables()
+                        
+                with tf.variable_scope("RNNConv2D"): 
+                    (output, state) = lstm_cell(input_, state)
+
+                dec_output = tt.network.conv2d("Conv-Reduce", output, 1,
                                                3, 3, 1, 1,
                                                weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
                                                bias_init=0.1,
                                                regularizer=tf.contrib.layers.l2_regularizer(LAMBDA))
-            tf.get_variable_scope().reuse_variables()
-        
-    
-    model_outputs = tf.pack(dec_outputs, axis=1)
+                _input = dec_output
+                dec_outputs.append(dec_output)
+
+            model_outputs = tf.pack(dec_outputs, axis=1)
     
     return model_outputs
 
