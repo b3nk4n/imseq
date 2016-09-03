a53351224caa099bf80fd82d1cae7d6df229438c
bsautermeister <benjamin.sautermeister@tum.de>
2016-09-03 13:39:52 +0200

    Refactored model to use new abstract class

--- conv-imseq/model/conv_lstmconv2d_encoder_decoder.py
+++ conv-imseq/model/conv_lstmconv2d_encoder_decoder.py
@@ -2,58 +2,58 @@ import tensortools as tt
 import tensorflow as tf
 
 
-def _create_lstm_cell(height, width, layers, filters, ksize_input, ksize_hidden):
+def _create_lstm_cell(height, width, layers, filters, ksize_input, ksize_hidden, memory_device):
     """Helper method to create a LSTMConv2D cell with multiple layers with
        variables bound to CPU memory.
     """
     lstm_cell = tt.recurrent.BasicLSTMConv2DCell(height, width, filters,
                                                  ksize_input=ksize_input,
                                                  ksize_hidden=ksize_hidden,
-                                                 device='/cpu:0')
+                                                 device=memory_device)
     if layers > 1:
         lstm_cell = tt.recurrent.MultiRNNConv2DCell([lstm_cell] * layers)
     return lstm_cell
 
 
-def _conv_stack(x, reg_lambda, is_training):
+def _conv_stack(x, reg_lambda, is_training, memory_device):
     conv1 = tt.network.conv2d("Conv1", x, 32,
                               (3, 3), (2, 2),
                               weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
                               bias_init=0.1,
                               regularizer=tf.contrib.layers.l2_regularizer(reg_lambda),
                               activation=tf.nn.relu,
-                              device='/cpu:0')
-    with tf.device('/cpu:0'):
-        conv1 = tf.contrib.layers.batch_norm(conv1, is_training=is_training, scope="conv1_bn")
+                              device=memory_device)
+    #with tf.device('/cpu:0'):
+    #    conv1 = tf.contrib.layers.batch_norm(conv1, is_training=is_training, scope="conv1_bn")
     conv2 = tt.network.conv2d("Conv2", conv1, 64,
                               (3, 3), (1, 1),
                               weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
                               bias_init=0.1,
                               regularizer=tf.contrib.layers.l2_regularizer(reg_lambda),
                               activation=tf.nn.relu,
-                              device='/cpu:0')
-    with tf.device('/cpu:0'):
-        conv2 = tf.contrib.layers.batch_norm(conv2, is_training=is_training, scope="conv2_bn")
+                              device=memory_device)
+    #with tf.device('/cpu:0'):
+    #    conv2 = tf.contrib.layers.batch_norm(conv2, is_training=is_training, scope="conv2_bn")
     conv3 = tt.network.conv2d("Conv3", conv2, 64,
                               (3, 3), (2, 2),
                               weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
                               bias_init=0.1,
                               regularizer=tf.contrib.layers.l2_regularizer(reg_lambda),
                               activation=tf.nn.relu,
-                              device='/cpu:0')
-    with tf.device('/cpu:0'):
-        conv3 = tf.contrib.layers.batch_norm(conv3, is_training=is_training, scope="conv3_bn")
+                              device=memory_device)
+    #with tf.device('/cpu:0'):
+    #    conv3 = tf.contrib.layers.batch_norm(conv3, is_training=is_training, scope="conv3_bn")
     return conv1, conv2, conv3
 
 
-def _deconv_stack(representation, conv1, conv2, reg_lambda, channels, is_training):
+def _deconv_stack(representation, conv1, conv2, reg_lambda, channels, is_training, memory_device):
     deconv1 = tt.network.conv2d_transpose("Deconv1", representation, 64,
                                           (3, 3), (2, 2),
                                           weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
                                           bias_init=0.1,
                                           regularizer=tf.contrib.layers.l2_regularizer(reg_lambda),
                                           activation=tf.nn.relu,
-                                          device='/cpu:0')
+                                          device=memory_device)
     #with tf.device('/cpu:0'):
         #deconv1 = tf.contrib.layers.batch_norm(deconv1, is_training=is_training, scope="deconv1_bn")
     x = tf.concat(3, [deconv1, conv2])
@@ -63,7 +63,7 @@ def _deconv_stack(representation, conv1, conv2, reg_lambda, channels, is_trainin
                                           bias_init=0.1,
                                           regularizer=tf.contrib.layers.l2_regularizer(reg_lambda),
                                           activation=tf.nn.relu,
-                                          device='/cpu:0')
+                                          device=memory_device)
     #with tf.device('/cpu:0'):
         #deconv2 = tf.contrib.layers.batch_norm(deconv2, is_training=is_training, scope="deconv2_bn")
     x = tf.concat(3, [deconv2, conv1])
@@ -72,33 +72,28 @@ def _deconv_stack(representation, conv1, conv2, reg_lambda, channels, is_trainin
                                           weight_init=tf.contrib.layers.xavier_initializer_conv2d(),
                                           bias_init=0.1,
                                           regularizer=tf.contrib.layers.l2_regularizer(reg_lambda),
-                                          device='/cpu:0')
+                                          activation=tf.nn.sigmoid,
+                                          device=memory_device)
     return deconv1, deconv2, deconv3
 
 
-class ConvLSTMConv2DDecoderEncoderModel(tt.model.AbstractModel):
-    """Model that uses the LSTM decoder/encoder architecture
-       with LSTMConv2D instead of normal LSTM cells that processes
-       the convoluted representations. 
-       
-       References: N. Srivastava et al.
-                   http://arxiv.org/abs/1502.04681
-    """
-    def __init__(self, inputs, targets, reg_lambda=5e-4,
-                 lstm_layers=1, lstm_ksize_input=(7, 7), lstm_ksize_hidden=(7,7),
-                 scope=None,
-                 is_training=False):
+    
+class ConvLSTMConv2DDecoderEncoderModel(tt.model.AbstractModel):    
+    def __init__(self, reg_lambda=0.0,
+                 lstm_layers=1, lstm_ksize_input=(7, 7), lstm_ksize_hidden=(7,7)):
         self._lstm_layers = lstm_layers
         self._lstm_ksize_input = lstm_ksize_input
         self._lstm_ksize_hidden = lstm_ksize_hidden
-        self._scope = scope  # TODO: move to base class (call is device scope?)
-        self._is_training = is_training  # TODO: move to base class
-        super(ConvLSTMConv2DDecoderEncoderModel, self).__init__(inputs, targets, reg_lambda)
-    
-    @tt.utils.attr.lazy_property
-    def predictions(self):
-        input_seq = tf.transpose(self._inputs, [1, 0, 2, 3, 4])
-        input_seq = tf.split(0, self.input_shape[1], input_seq)
+        super(ConvLSTMConv2DDecoderEncoderModel, self).__init__(reg_lambda)
+        
+    @tt.utils.attr.override
+    def inference(self, inputs, targets, is_training=True,
+                  device_scope=None, memory_device=None):
+        input_shape = inputs.get_shape().as_list()
+        target_shape = targets.get_shape().as_list()
+        
+        input_seq = tf.transpose(inputs, [1, 0, 2, 3, 4])
+        input_seq = tf.split(0, input_shape[1], input_seq)
         input_seq = [tf.squeeze(i, (0,)) for i in input_seq]
         
         with tf.variable_scope("ConvStack") as varscope:
@@ -108,7 +103,8 @@ class ConvLSTMConv2DDecoderEncoderModel(tt.model.AbstractModel):
                     varscope.reuse_variables()
 
                 # we only need the very last c1,c2. Override the previous
-                (c1, c2, c3) = _conv_stack(input_seq[i], self.reg_lambda, self._is_training)
+                (c1, c2, c3) = _conv_stack(input_seq[i], self.reg_lambda,
+                                           is_training, memory_device)
                 conv_input_seq.append(c3)
             
         representation_shape = c3.get_shape().as_list()
@@ -116,7 +112,8 @@ class ConvLSTMConv2DDecoderEncoderModel(tt.model.AbstractModel):
         with tf.variable_scope("encoder-lstm"):
             lstm_cell = _create_lstm_cell(representation_shape[1], representation_shape[2],
                                           self._lstm_layers, representation_shape[3], 
-                                          self._lstm_ksize_input, self._lstm_ksize_hidden)
+                                          self._lstm_ksize_input, self._lstm_ksize_hidden,
+                                          memory_device)
             _, enc_state = tt.recurrent.rnn_conv2d(lstm_cell, conv_input_seq, initial_state=None)  
 
         # on test/eval: use previously generated frame
@@ -124,10 +121,11 @@ class ConvLSTMConv2DDecoderEncoderModel(tt.model.AbstractModel):
         with tf.variable_scope('decoder-lstm') as varscope:
             lstm_cell = _create_lstm_cell(representation_shape[1], representation_shape[2],
                                           self._lstm_layers, representation_shape[3], 
-                                          self._lstm_ksize_input, self._lstm_ksize_hidden)
+                                          self._lstm_ksize_input, self._lstm_ksize_hidden,
+                                          memory_device)
             
             rep_outputs, _ = tt.recurrent.rnn_conv2d_roundabout(lstm_cell, conv_input_seq[-1],
-                                                                sequence_length=self.output_shape[1],
+                                                                sequence_length=target_shape[1],
                                                                 initial_state=enc_state)
             
         with tf.variable_scope("DeconvStack") as varscope:
@@ -139,18 +137,15 @@ class ConvLSTMConv2DDecoderEncoderModel(tt.model.AbstractModel):
                     
                 # we only need the very last c1,c2. Override the previous
                 (dc1, dc2, dc3) = _deconv_stack(rep_outputs[i], inputs[0], inputs[1],
-                                                self.reg_lambda, self.input_shape[4],
-                                                self._is_training)
+                                                self.reg_lambda, input_shape[4],
+                                                is_training, memory_device)
                 deconv_output_seq.append(dc3)
                 inputs = (dc2, dc1)
 
-        return tf.pack(deconv_output_seq, axis=1)
-
-    @tt.utils.attr.lazy_property
-    def loss(self):
-        predictions = self.predictions
-        # Calculate the average squared loss per image across the batch
-        loss = tf.reduce_mean(
-            tf.reduce_sum(tf.pow(tf.sub(predictions, self._targets), 2), reduction_indices=[2,3,4]),
-            name="squared_loss")
-        return loss
+        packed_result = tf.pack(deconv_output_seq, axis=1)
+        packed_result.set_shape(target_shape)
+        return packed_result
+    
+    @tt.utils.attr.override
+    def loss(self, predictions, targets):
+        return tt.loss.bce(predictions, targets)
